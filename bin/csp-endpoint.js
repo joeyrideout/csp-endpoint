6b4a0e4b74d9b52db72d1ed1c9341b2237032418
Joey Rideout <joeyrideout@users.noreply.github.com>
2018-05-18 21:57:18 -0400

    Update csp-endpoint.js

--- bin/csp-endpoint.js
+++ bin/csp-endpoint.js
@@ -7,31 +7,22 @@ var express = require('express');
 var app = express();
 app.use(endpoint.parser);
 
-var opts = require('nomnom')
-  .option('path', {
-    default: '/',
-    help: 'The path of the endpoint to collect reports on'
-  })
-  .option('port', {
-    default: '3000',
-    help: 'The port of the endpoint to collect reports on'
-  }).parse();
+var opts = {
+  "port": 3000,
+  "path": "/",
+}
 
 app.post(opts.path, function(req, res){
   var report = JSON.parse(req.report);
-
-  if (opts.time) {
+/*if (opts.time) {
     report.ts = new Date();
   }
-
   if (opts.headers) {
     report.headers = JSON.stringify(req.headers);
   }
-
   if (opts.ip) {
     report.ip = req.ip;
-  }
-
+  }*/
   console.log(JSON.stringify(report));
   res.send('Thanks!');
 });
